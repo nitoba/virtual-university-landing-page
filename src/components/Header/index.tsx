3d89efeca40b62615c728cd467f4422c584acbf4
Bruno Alves <bruno@sthorm.io>
2022-07-03 01:29:26 -0300

    feat: add header mobile responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,39 +1,93 @@
-import styles from "./styles.module.scss";
 import logo from "../../assets/logo.svg";
+import menu from "../../assets/icons/menu.svg";
+import close from "../../assets/icons/close.svg";
+
 import { Button } from "../Button";
+import { useRef, useState } from "react";
+
+import styles from "./styles.module.scss";
+
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
   return (
-    <header className={styles.container}>
-      <div className={styles.content}>
-        <img className={styles.logo} src={logo} alt="Credischool logo" />
-        <nav className={styles.navContainer}>
-          <ul className={styles.listOfItems}>
-            <li className={styles.item}>
-              <a href="#">Home</a>
-            </li>
+    <>
+      {isOpen && (
+        <div ref={menuRef} className={styles.menuMobile}>
+          <img
+            onClick={async () => {
+              menuRef.current?.classList.add(styles.closeMenu);
+              await new Promise((resolve) => setTimeout(resolve, 400));
+              setIsOpen(false);
+            }}
+            className={styles.closeIcon}
+            src={close}
+            alt="close icon"
+          />
+          <nav className={styles.navContainer}>
+            <ul className={styles.listOfItems}>
+              <li className={styles.item}>
+                <a href="#">Home</a>
+              </li>
 
-            <li className={styles.item}>
-              <a href="#">Campus</a>
-            </li>
+              <li className={styles.item}>
+                <a href="#">Campus</a>
+              </li>
 
-            <li className={styles.item}>
-              <a href="#">Education</a>
-            </li>
+              <li className={styles.item}>
+                <a href="#">Education</a>
+              </li>
 
-            <li className={styles.item}>
-              <a href="#">Alumni</a>
-            </li>
+              <li className={styles.item}>
+                <a href="#">Alumni</a>
+              </li>
 
-            <li className={styles.item}>
-              <a href="#">About</a>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.actionButtons}>
-          <button className={styles.login}>Login</button>
-          <Button variant="red" title="Register" />
+              <li className={styles.item}>
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </div>
-    </header>
+      )}
+      <header className={styles.container}>
+        <div className={styles.content}>
+          <img className={styles.logo} src={logo} alt="Credischool logo" />
+          <img
+            onClick={() => setIsOpen(true)}
+            className={styles.menuIcon}
+            src={menu}
+            alt="Menu icon"
+          />
+
+          <nav className={styles.navContainer}>
+            <ul className={styles.listOfItems}>
+              <li className={styles.item}>
+                <a href="#">Home</a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#">Campus</a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#">Education</a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#">Alumni</a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.actionButtons}>
+            <button className={styles.login}>Login</button>
+            <Button variant="red" title="Register" />
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
